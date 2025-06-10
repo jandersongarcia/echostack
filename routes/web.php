af1ccfcf53723184ae9ec5685e4929f373a02295
Janderson Garcia <70637518+jandersongarcia@users.noreply.github.com>
2025-06-10 16:42:49 -0400

    Excluído arquivos extras

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,3 @@ $router->map('GET', '/health', function() use ($database, $logger) {
     $controller = new HealthController($database, $logger);
     return $controller->check();
 });
-
-// Rotas automáticas para o CRUD de todo
-
-
-// Rotas automáticas para o CRUD de todo
-$router->map('GET', '/todo', 'App\\Controllers\\TodoController@index');
-$router->map('GET', '/todo/[i:id]', 'App\\Controllers\\TodoController@show');
-$router->map('POST', '/todo', 'App\\Controllers\\TodoController@store');
-$router->map('PUT', '/todo/[i:id]', 'App\\Controllers\\TodoController@update');
-$router->map('DELETE', '/todo/[i:id]', 'App\\Controllers\\TodoController@destroy');
